23d4a31442e55bc3ec722aeddfb73d89d7ba8488
7teen <71920969+17teen@users.noreply.github.com>
2021-01-23 07:32:10 +0000

    17

--- Cycle Mode/cycleparty.js
+++ Cycle Mode/cycleparty.js
@@ -1,5 +1,5 @@
 // https://github.com/17teen
-// Discord: thug angel#3868
+// Discord: 7teen#3868
 // https://github.com/Vissage
 
 const RPC = require("discord-rpc");
@@ -63,7 +63,7 @@ rpc.on("ready", () => {
           ░░░░██▓▓▓▓▓▓████▒▒                                  
         ░░░░░░██▓▓▒▒▓▓▓▓▓▓▓▓   17                               
         ░░░░░░░░▒▒▒▒▒▒▓▓████                                  
-      ░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒             Made by 7teen#1464                        
+      ░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒             Made by 7teen#3868                       
       ░░░░░░▒▒▒▒▓▓▒▒░░                                        
     ░░░░▒▒▒▒▒▒▒▒▒▒                                            
     ░░░░▒▒▒▒▒▒▒▒                                          
