5072809f739ecdcadc911a42f340b7b2544c008a
7teen <71920969+17teen@users.noreply.github.com>
2021-01-23 07:19:07 +0000

    17

--- launch.js
+++ launch.js
@@ -16,5 +16,5 @@ const title = String.raw`
 console.log(title)
 
 console.log(``);
-console.log(`                                          Made by thug angel#3868`)
+console.log(`                                          Made by 7teen#3868`)
 console.log(``);
