9f429b92eab51b66e422b1a9f27d8133f5aa19c5
meow6969 <75815262+meow6969@users.noreply.github.com>
2021-04-30 18:58:45 +0000

    add files for linux users

---
+++ Cycle Mode/Cycle Mode.sh
@@ -0,0 +1,7 @@
+echo Rich Presence App: Cycle by 7teen;
+while true;
+	do
+	clear;
+	node cycle.js;
+	read -s -n 1 -p "Press any key to continue . . ."
+	done
---
+++ Cycle Mode/Cycle Party Mode.sh
@@ -0,0 +1,7 @@
+echo Rich Presence App: Cycle Party by 7teen;
+while true;
+	do
+	clear;
+	node cycleparty.js;
+	read -s -n 1 -p "Press any key to continue . . .";
+	done;
