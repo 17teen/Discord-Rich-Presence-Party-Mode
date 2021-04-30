fdbff2acfb96572dcfb4b1d6e323afa8cee4375b
meow6969 <75815262+meow6969@users.noreply.github.com>
2021-04-30 19:02:44 +0000

    Update Party Mode.sh

--- Party Mode/Party Mode.sh
+++ Party Mode/Party Mode.sh
@@ -2,6 +2,6 @@ echo Rich Presence App: Party by 7teen;
 while true;
 	do
 	node party.js;
-	read -s -n 1 -p "Press any key to continue"
+	read -s -n 1 -p "Press any key to continue. . . ";
 	done;
 done;
